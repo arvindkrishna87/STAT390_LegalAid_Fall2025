8e8f3f5632c66ae0c4584845396217cbb4c61204
carolinecorr <carolinecorr2026@u.northwestern.edu>
2025-10-07 11:02:56 -0500

    nikole's graph

--- Internal work of Teams/EDA_simple_2/extra_plot_nk.R
+++ Internal work of Teams/EDA_simple_2/extra_plot_nk.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
+library(scales)
 
 # read in data
 all_calls <- read_csv('data/combined_All_Call.csv')
@@ -13,7 +14,7 @@ calls_by_hour_all <-
   filter(duration > 0, direction == "TERMINATING", pstn_vendor_name == "CallTower") %>%
   mutate(hour = hour(report_time)) %>%
   count(hour) %>%
-  rename(All_Calls = n)
+  mutate(dataset = 'All Calls')
 
 calls_by_hour_car <-
   car %>%
@@ -22,11 +23,17 @@ calls_by_hour_car <-
     activity_start_timestamp = format(activity_start_timestamp, "%Y-%m-%d %H:%M:%S"),
     hour = hour(activity_start_timestamp)) %>%
   count(hour) %>%
-  rename(CAR = n)
+  mutate(dataset = "CAR")
 
 calls_hour_comp <-
   full_join(calls_by_hour_all, calls_by_hour_car, by = "hour") %>%
   pivot_longer(cols = c(All_Calls, CAR), names_to = "Dataset", values_to = "Count")
+
+hour_share <-
+  bind_rows(calls_by_hour_all, calls_by_hour_car) |>
+  group_by(dataset) |>
+  mutate(pct = n / sum(n)) |>
+  ungroup()
 
 ggplot(calls_hour_comp, aes(x = hour, y = Count, color = Dataset)) +
   geom_line(linewidth = 1) +
@@ -38,3 +45,19 @@ ggplot(calls_hour_comp, aes(x = hour, y = Count, color = Dataset)) +
     color = "Dataset"
   ) +
   theme_minimal()
+
+ggplot(hour_share, aes(hour, pct, color = dataset)) +
+  geom_line(linewidth = 1.2) +
+  geom_point(size = 2) +
+  #scale_x_continuous(breaks = 0.23) +
+  scale_y_continuous(labels = percent_format(accuracy = 1)) +
+  labs(
+    title = 'Hourly Share of Inbound Calls',
+    subtitle = 'Each line shows distribution within dataset',
+    x = 'Hour of Day (24-hour)',
+    y = 'Share of Calls',
+    color = 'Dataset'
+  ) +
+  theme_minimal(base_size = 13) +
+  theme(legend.position = 'top',
+        panel.grid.minor = element_blank())
